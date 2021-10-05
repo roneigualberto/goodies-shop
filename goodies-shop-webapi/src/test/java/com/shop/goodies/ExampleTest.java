2af6b7efbc5c67f399f5c51b8c1dc1b3392e3411
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2021-10-05 18:14:25 -0400

    Setup Tests

---
+++ goodies-shop-webapi/src/test/java/com/shop/goodies/ExampleTest.java
@@ -0,0 +1,12 @@
+package com.shop.goodies;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+    @Test
+    public void test() {
+        Assertions.assertNull(null);
+    }
+}
