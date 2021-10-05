cd6cc668358ab96ce3454006675136b389e0cb99
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2021-10-05 15:58:35 -0400

    Setup Backend

---
+++ goodies-shop-webapi/src/test/java/com/shop/goodies/GoodiesShopWebapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.goodies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodiesShopWebapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
