7b1f82cd298b7e80be36c29c2545e84d5e474914
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2021-10-06 12:56:23 -0400

    Setup profile test

--- goodies-shop-webapi/src/integration-test/java/com/shop/goodies/BaseIntegrationTest.java
+++ goodies-shop-webapi/src/integration-test/java/com/shop/goodies/BaseIntegrationTest.java
@@ -5,12 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-public class ExampleIT {
+@ActiveProfiles("test")
+public class BaseIntegrationTest {
 
     @Test
     public void test() {
